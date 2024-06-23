6c091e8d68da628bfa121550109ec95dcf6213e8
ken_thn <thanaton_b@cmu.ac.th>
2024-06-23 20:37:41 +0700

    update

--- dags/dag.py
+++ dags/dag.py
@@ -4,45 +4,39 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-
+from airflow.utils.dates import days_ago
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from pipelines.aws_s3_pipeline import upload_s3_pipeline
-from pipelines.reddit_pipeline import reddit_pipeline
+
+from pipelines.alphavantage_pipeline import alphavantage_pipeline
+
 
 default_args = {
-    'owner': 'Yusuf Ganiyu',
-    'start_date': datetime(2023, 10, 22)
+    'owner': 'airflow',
+    'start_date': days_ago(0),
+    'depends_on_past': False,
+    'start_date': datetime(2024, 6, 22),    
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
 }
 
 file_postfix = datetime.now().strftime("%Y%m%d")
 
 dag = DAG(
-    dag_id='etl_reddit_pipeline',
+    dag_id='etl_income_statement_pipeline',
     default_args=default_args,
-    schedule_interval='@daily',
     catchup=False,
     tags=['reddit', 'etl', 'pipeline']
 )
 
-# extraction from reddit
 extract = PythonOperator(
-    task_id='reddit_extraction',
-    python_callable=reddit_pipeline,
+    task_id='income_extraction',
+    python_callable=alphavantage_pipeline,
     op_kwargs={
-        'file_name': f'reddit_{file_postfix}',
-        'subreddit': 'dataengineering',
-        'time_filter': 'day',
-        'limit': 100
+        'file_name': f'income_{file_postfix}',
     },
     dag=dag
 )
 
-# upload to s3
-upload_s3 = PythonOperator(
-    task_id='s3_upload',
-    python_callable=upload_s3_pipeline,
-    dag=dag
-)
-
-extract >> upload_s3
+extract 
